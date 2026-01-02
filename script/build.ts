d05f23ea1ae02220e3cf3fd4e637c42eaa920985
gitBakr <bakrbackend@gmail.com>
2026-01-02 21:44:20 +0100

    fix: Correction alias esbuild avec resolve path absolu

--- script/build.ts
+++ script/build.ts
@@ -1,6 +1,7 @@
 import { build as esbuild } from "esbuild";
 import { build as viteBuild } from "vite";
 import { rm, readFile } from "fs/promises";
+import { resolve } from "path";
 
 // server deps to bundle to reduce openat(2) syscalls
 // which helps cold start times
@@ -60,8 +61,9 @@ async function buildAll() {
     minify: true,
     external: externals,
     logLevel: "info",
+    resolveExtensions: [".ts", ".js"],
     alias: {
-      "@shared": "./shared",
+      "@shared": resolve(process.cwd(), "shared"),
     },
   });
 }
